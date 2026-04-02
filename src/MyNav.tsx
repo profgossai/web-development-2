9336e4deaed39936794ebfed3c8c0838b1fec1a8
mgossai <profgossai@gmail.com>
2026-04-01 20:43:55 -0400

    UPdate with table

---
+++ src/MyNav.tsx
@@ -0,0 +1,28 @@
+import Home from "./Home";
+import About from "./About";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
+function MyNav() {
+  return (
+    <BrowserRouter>
+        <SetNav />
+        <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+    </BrowserRouter>
+    );
+}
+
+function SetNav() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default MyNav;
